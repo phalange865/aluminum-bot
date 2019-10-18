19d07d9ca1ce594faf8d25409ec3150285921776
user <User@LPC-0003.pjlhuillier.local>
2019-10-18 17:06:59 +0800

    changes

--- commands/replies.js
+++ commands/replies.js
@@ -1,27 +1,32 @@
 const discord = require("discord.js");
 const fs = require('fs');
+const MongoClient = require('mongodb').MongoClient
+const uri = process.env.MONGODB_URI
 
 module.exports.run = async (bot, message, args) => {
+
   const msg = message.content.match(/\((.*)\)/)[1];
   const data = msg.split(",")
 
-  let newData = {
-    wer: data[0],
-    bi: data[1],
-    aluminum: data[2],
-    antimony: data[3],
-    argon: data[4],
-    potassium: data[5]
-  };
-
-  let jsonData = JSON.stringify(newData, null, 2);
-
-  fs.writeFile('./json/replies.json', jsonData, (err) => {
-    if (err) throw err;
-     return message.channel.send("done!")
+  MongoClient.connect(uri, function(err, db) {
+    if (err) {
+      console.log('Error occurred while connecting to MongoDB Atlas...\n',err);
+    }
+    else{
+      var dbo = db.db("heroku_wmh62vg1");
+      var collection = dbo.collection("replies");
+      var myquery = { "_id": "1" };
+      var newvalues = { $set: {  wer: data[0], bi: data[1], aluminum: data[2], antimony: data[3], argon: data[4],  potassium: data[5] } };
+      dbo.collection("customers").updateOne(myquery, newvalues, function(err, res) {
+        if (err) throw err;
+        console.log("1 document updated");
+        db.close();
+      });
+      db.close();
+    }
   });
 };
 
 module.exports.help = {
-name: "replies"
+  name: "replies"
 };
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const MongoClient = require('mongodb').MongoClient
-const uri = "mongodb://heroku_wmh62vg1:oa6ab4v6f6cf7tj65hga56ng0g@ds335668.mlab.com:35668/heroku_wmh62vg1"
+const uri =
 
 MongoClient.connect(uri, function(err, db) {
   if (err) {
@@ -8,5 +8,4 @@ MongoClient.connect(uri, function(err, db) {
     console.log("Database created!");
     db.close();
   }
-
 });
